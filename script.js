11489a5aa4035d2a78dea0329f4f1a20f4321428
KatiaArruda <111231609+KatiaArruda@users.noreply.github.com>
2023-02-22 17:18:11 -0300

    Nova alteração
    Nova alteração na branch

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-window.document.getElementById("#dados");
+function carregar() {
+  const msg = window.document.getElementById("#msg");
+  const img = window.document.getElementById("#imagem");
+  const data = new Date();
+  const hora = data.getHours();
+  msg.innerHTML = 'Bom dia!';
+
+  if (hora >= 0 && hora < 12) {
+    //Bom dia!
+    img.src = "fotodaManhaAF.png";
+  } else if (hora >= 12 && hora < 18) {
+    //Boa tarde
+    img.src = "FotodaTardeAF";
+  } else {
+    //Boa noite
+    img.src = "FotodaNoiteAF";
+  }
+}
+
+
