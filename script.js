5bc1d8ff07bbd3f7c44ee21922036ac896db199d
KatiaArruda <111231609+KatiaArruda@users.noreply.github.com>
2023-02-25 22:43:15 -0300

    Ajuste final no javascript
    Alteração no javascript

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 function carregar() {
-  const msg = window.document.getElementById("#msg");
-  const img = window.document.getElementById("#imagem");
-  const data = new Date();
-  //const hora = data.getHours();
-  const hora = 8
-  msg.innerHTML = 'Bom dia!';
+  const msg = window.document.getElementById('msg')
+  const img = window.document.getElementById('imagem')
+  const data= new Date()
+  const hora = data.getHours()
+  msg.innerHTML =  hora + 'hs'
 
-  if (hora >= 0 && hora < 12) {
-    //Bom dia!
-    img.src = '';
-  } else if (hora >= 12 && hora < 18) {
-    //Boa tarde
-    img.src = "FotodaTardeAF";
-  } else {
-    //Boa noite
-    img.src = "FotodaNoiteAF";
-  }
 }
-
-
